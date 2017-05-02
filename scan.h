85401d087c381e422d3a6430cb33da2345ab04fa
Stefan Kemnitz <kemnitz.stefan@googlemail.com>
2017-05-02 07:48:35 +0000

    added code to understand cxx bool

--- scan.h
+++ scan.h
@@ -214,6 +214,7 @@ private:
 	__isl_give pet_expr *extract_expr(clang::BinaryOperator *expr);
 	__isl_give pet_expr *extract_expr(clang::ImplicitCastExpr *expr);
 	__isl_give pet_expr *extract_expr(clang::IntegerLiteral *expr);
+	__isl_give pet_expr *extract_expr(clang::CXXBoolLiteralExpr *expr);
 	__isl_give pet_expr *extract_expr(clang::EnumConstantDecl *expr);
 	__isl_give pet_expr *extract_expr(clang::FloatingLiteral *expr);
 	__isl_give pet_expr *extract_expr(clang::ParenExpr *expr);
